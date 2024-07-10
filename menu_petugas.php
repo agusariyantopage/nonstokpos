35c8ddbb65aa3b37819aee6443b363932e2caaf7
agusariyantopage <agus.arianto21@gmail.com>
2024-07-10 21:58:20 +0800

    Update Akses Laporan Ke Kasir

--- menu_petugas.php
+++ menu_petugas.php
@@ -15,16 +15,25 @@
                </a>
              </li>
              <li class="nav-item">
-                <a href="index.php?p=jurnal-tambah-biaya" class="nav-link">
-                  <i class="fas fa-hand-holding-usd nav-icon"></i>
-                  <p>Pengeluaran Kas</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="index.php?p=jurnal-tambah-biayatransfer" class="nav-link">
-                  <i class="fas fa-exchange-alt nav-icon"></i>
-                  <p>Pengeluaran Transfer</p>
-                </a>
-              </li>
+               <a href="index.php?p=jurnal-tambah-biaya" class="nav-link">
+                 <i class="fas fa-hand-holding-usd nav-icon"></i>
+                 <p>Pengeluaran Kas</p>
+               </a>
+             </li>
+             <li class="nav-item">
+               <a href="index.php?p=jurnal-tambah-biayatransfer" class="nav-link">
+                 <i class="fas fa-exchange-alt nav-icon"></i>
+                 <p>Pengeluaran Transfer</p>
+               </a>
+             </li>
            </ul>
+         </li>
+
+         <li class="nav-item">
+           <a href="index.php?p=laporan" class="nav-link">
+             <i class="nav-icon fas fa-print"></i>
+             <p>
+               Laporan
+             </p>
+           </a>
          </li>
